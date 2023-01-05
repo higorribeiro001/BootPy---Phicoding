e7eaabbc045def3cdd2969ec750ef4119f862bcb
higorribeiro001 <103210211+higorribeiro001@users.noreply.github.com>
2023-01-05 15:46:39 -0300

    Adição do m2a18

---
+++ m2a18.py
@@ -0,0 +1,8 @@
+# 1 print por linha
+# substuir espaço por "-"
+# utilizar o argumento "end" adcionar um PhiCoding no final da linha e tambem quebrar a linha
+
+print('É', 'preciso', 'ter', 'um', 'sonho', 'pra', 'se', 'adaptar', 'a', 'realidade', sep='-', end='-PhiCoding\n\r') 
+print('E', 'quanto', 'menor', 'é', 'a', 'adaptação', 'a', 'realidade', sep='-', end='-PhiCoding\n\r')
+print('Maior', 'é', 'a', 'necessidade', 'de', 'persistir', 'no', 'sonho', 'até', 'tu', 'conseguir', 'realizar', sep='-', end='-PhiCoding\n\r')
+print('A', 'realidade', 'é', 'um', 'glorioso', 'cemitério', 'de', 'sonhos', sep='-', end='-PhiCoding\n\r')
